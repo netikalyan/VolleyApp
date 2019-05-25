dfa150e7e58275a8790bc9730be4a013840dff4e
netikalyan <netikalyan@gmail.com>
2019-05-25 18:20:00 +0530

    Using WebView to render Google homepage transmitted using Volley

--- app/src/main/java/com/android/practise/volleyapp/MainActivity.java
+++ app/src/main/java/com/android/practise/volleyapp/MainActivity.java
@@ -2,12 +2,51 @@ package com.android.practise.volleyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String URL = "http://www.google.co.in";
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRequestQueue = Volley.newRequestQueue(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final WebView webView = findViewById(R.id.text);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                webView.loadData(response, "text/html", "en");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                webView.loadData(error.getLocalizedMessage(), "text/plain", "en");
+            }
+        });
+        stringRequest.setTag(TAG);
+        mRequestQueue.add(stringRequest);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(TAG);
+        }
     }
 }
